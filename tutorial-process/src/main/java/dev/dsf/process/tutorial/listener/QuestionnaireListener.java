ae8b2c5fd8e12268e142432ac7d29f517238273f
Jan Böhringer <jan.boehringer@hs-heilbronn.de>
2024-08-30 16:34:27 +0200

    QuestionnaireResponse.item.text must always have the same value as the originating Questionnaire's value. Current fix is to also change the Questionnaire resource's item.text value.

--- tutorial-process/src/main/java/dev/dsf/process/tutorial/listener/QuestionnaireListener.java
+++ tutorial-process/src/main/java/dev/dsf/process/tutorial/listener/QuestionnaireListener.java
@@ -1,18 +1,28 @@
 package dev.dsf.process.tutorial.listener;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
 import org.camunda.bpm.engine.delegate.DelegateTask;
+import org.hl7.fhir.r4.model.Bundle;
+import org.hl7.fhir.r4.model.Questionnaire;
 import org.hl7.fhir.r4.model.QuestionnaireResponse;
 import org.springframework.beans.factory.InitializingBean;
 
 import dev.dsf.bpe.v1.ProcessPluginApi;
 import dev.dsf.bpe.v1.activity.DefaultUserTaskListener;
 import dev.dsf.process.tutorial.ConstantsTutorial;
+import dev.dsf.process.tutorial.TutorialProcessPluginDefinition;
 
 public class QuestionnaireListener extends DefaultUserTaskListener implements InitializingBean
 {
+	ProcessPluginApi api;
 	public QuestionnaireListener(ProcessPluginApi api)
 	{
 		super(api);
+		this.api = api;
 	}
 
 	@Override
@@ -24,8 +34,33 @@ public class QuestionnaireListener extends DefaultUserTaskListener implements In
 	@Override
 	protected void beforeQuestionnaireResponseCreate(DelegateTask userTask, QuestionnaireResponse beforeCreate)
 	{
-		String question = (String) userTask.getExecution().getVariable(ConstantsTutorial.CODESYSTEM_TUTORIAL_VALUE_BINARY_QUESTION);
+		Optional<Questionnaire> optionalQuestionnaire = findQuestionnaire();
+		if (optionalQuestionnaire.isPresent())
+		{
+			String question = (String) userTask.getExecution()
+					.getVariable(ConstantsTutorial.CODESYSTEM_TUTORIAL_VALUE_BINARY_QUESTION);
 
-		beforeCreate.getItem().stream().filter(item -> item.getLinkId().equals("vote")).findFirst().ifPresent(item -> item.setText(question));
+			Questionnaire questionnaire = optionalQuestionnaire.get();
+			questionnaire.getItem().stream().filter(item -> item.getLinkId().equals("vote")).findFirst().ifPresent(item -> item.setText(question));
+			updateQuestionnaire(questionnaire);
+
+			beforeCreate.getItem().stream().filter(item -> item.getLinkId().equals("vote")).findFirst()
+					.ifPresent(item -> item.setText(question));
+		}
+	}
+
+	private Optional<Questionnaire> findQuestionnaire()
+	{
+		Map<String, List<String>> searchParams = new HashMap<>();
+		searchParams.put("url", List.of(ConstantsTutorial.QUESTIONNAIRE_USER_VOTE_URL));
+		searchParams.put("version", List.of(TutorialProcessPluginDefinition.VERSION));
+		Bundle result = api.getFhirWebserviceClientProvider().getLocalWebserviceClient().searchWithStrictHandling(Questionnaire.class, searchParams);
+		if (result.getTotal() < 1) return Optional.empty();
+		return result.getEntry().stream().findFirst().map(Questionnaire.class::cast);
+	}
+
+	private void updateQuestionnaire(Questionnaire questionnaire)
+	{
+		api.getFhirWebserviceClientProvider().getLocalWebserviceClient().update(questionnaire);
 	}
 }
