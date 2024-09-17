174bff2decbf1d0398d6c0cda63dd5bf3e4b5d0a
Jan Böhringer <jan.boehringer@hs-heilbronn.de>
2024-09-17 18:19:55 +0200

    Added to commit history in case it's needed again

--- tutorial-process/src/main/java/dev/dsf/process/tutorial/service/CreateQuestionnaire.java
+++ tutorial-process/src/main/java/dev/dsf/process/tutorial/service/CreateQuestionnaire.java
@@ -1,43 +1,46 @@
-package dev.dsf.process.tutorial.listener;
+package dev.dsf.process.tutorial.service;
+
+import static dev.dsf.process.tutorial.ConstantsTutorial.CODESYSTEM_TUTORIAL;
+import static dev.dsf.process.tutorial.ConstantsTutorial.CODESYSTEM_VOTING_PROCESS_VALUE_BINARY_QUESTION;
 
 import java.time.Instant;
 import java.util.Date;
-import java.util.UUID;
 
-import org.camunda.bpm.engine.delegate.DelegateTask;
+import org.camunda.bpm.engine.delegate.BpmnError;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.hl7.fhir.r4.model.Coding;
 import org.hl7.fhir.r4.model.Enumerations;
 import org.hl7.fhir.r4.model.Meta;
 import org.hl7.fhir.r4.model.Questionnaire;
+import org.hl7.fhir.r4.model.Task;
 
 import dev.dsf.bpe.v1.ProcessPluginApi;
+import dev.dsf.bpe.v1.activity.AbstractServiceDelegate;
+import dev.dsf.bpe.v1.constants.CodeSystems;
+import dev.dsf.bpe.v1.variables.Variables;
 import dev.dsf.process.tutorial.ConstantsTutorial;
 import dev.dsf.process.tutorial.TutorialProcessPluginDefinition;
 
-public class QuestionnaireListener extends CustomUserTaskListener
+public class CreateQuestionnaire extends AbstractServiceDelegate
 {
-	ProcessPluginApi api;
-	public QuestionnaireListener(ProcessPluginApi api)
+	public CreateQuestionnaire(ProcessPluginApi api)
 	{
 		super(api);
-		this.api = api;
 	}
 
 	@Override
-	protected void beforeQuestionnaireResponseCreate(DelegateTask userTask, Questionnaire questionnaire)
+	protected void doExecute(DelegateExecution execution, Variables variables) throws BpmnError, Exception
 	{
-		String question = (String) userTask.getExecution()
-				.getVariable(ConstantsTutorial.CODESYSTEM_TUTORIAL_VALUE_BINARY_QUESTION);
-		questionnaire.getItem().stream().filter(item -> item.getLinkId().equals("vote")).findFirst().ifPresent(item -> item.setText(question));
+		Task startTask = variables.getStartTask();
+		String question = api.getTaskHelper().getFirstInputParameterStringValue(startTask,
+				CODESYSTEM_TUTORIAL, CODESYSTEM_VOTING_PROCESS_VALUE_BINARY_QUESTION).get();
+		String businessKey = api.getTaskHelper().getFirstInputParameterStringValue(startTask, CodeSystems.BpmnMessage.URL, CodeSystems.BpmnMessage.Codes.BUSINESS_KEY).get();
+		Questionnaire questionnaire = createQuestionnaire(question, businessKey);
+		Questionnaire created = uploadQuestionnaire(questionnaire);
+		variables.setResource(ConstantsTutorial.PROCESS_VARIABLE_QUESTIONNAIRE, created);
 	}
 
-	@Override
-	protected Questionnaire provideQuestionnaire()
-	{
-		return getQuestionnaire();
-	}
-
-	private Questionnaire getQuestionnaire()
+	private Questionnaire createQuestionnaire(String question, String businessKey)
 	{
 		Questionnaire questionnaire = new Questionnaire();
 		questionnaire.setMeta(
@@ -45,8 +48,7 @@ public class QuestionnaireListener extends CustomUserTaskListener
 						.addProfile("http://dsf.dev/fhir/StructureDefinition/questionnaire|1.5.0")
 						.addTag(new Coding().setSystem("http://dsf.dev/fhir/CodeSystem/read-access-tag").setCode("ALL"))
 		);
-		questionnaire.setUrl("http://dsf.dev/fhir/Questionnaire/user-vote");
-		questionnaire.setId(UUID.randomUUID().toString());
+		questionnaire.setUrl("http://dsf.dev/fhir/Questionnaire/voting-process-" + businessKey);
 
 		questionnaire.setVersion(new TutorialProcessPluginDefinition().getResourceVersion());
 		questionnaire.setDate(Date.from(Instant.now()));
@@ -69,10 +71,15 @@ public class QuestionnaireListener extends CustomUserTaskListener
 		Questionnaire.QuestionnaireItemComponent voteItem = new Questionnaire.QuestionnaireItemComponent();
 		voteItem.setLinkId("vote");
 		voteItem.setType(Questionnaire.QuestionnaireItemType.BOOLEAN);
-		voteItem.setText("Placeholder");
+		voteItem.setText(question);
 		voteItem.setRequired(true);
 		questionnaire.addItem(voteItem);
 
 		return questionnaire;
+	}
+
+	private Questionnaire uploadQuestionnaire(Questionnaire questionnaire)
+	{
+		return api.getFhirWebserviceClientProvider().getLocalWebserviceClient().withRetryForever(60000).create(questionnaire);
 	}
 }
