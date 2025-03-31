32f1bf54307bb6b9d813be871c47c943573101b8
Jan Böhringer <jan.boehringer@hs-heilbronn.de>
2025-03-31 19:11:23 +0200

    Now has to implement MessageIntermediateThrowingEvent

--- tutorial-process/src/main/java/dev/dsf/process/tutorial/message/HelloCosMessage.java
+++ tutorial-process/src/main/java/dev/dsf/process/tutorial/message/HelloCosMessage.java
@@ -10,13 +10,13 @@ import org.hl7.fhir.r4.model.StringType;
 import org.hl7.fhir.r4.model.Task;
 
 import dev.dsf.bpe.v2.ProcessPluginApi;
+import dev.dsf.bpe.v2.activity.MessageIntermediateThrowEvent;
 import dev.dsf.bpe.v2.activity.values.SendTaskValues;
 import dev.dsf.bpe.v2.variables.Target;
 import dev.dsf.bpe.v2.variables.Variables;
-import dev.dsf.bpe.v2.activity.MessageEndEvent;
 
 // Only needed for exercise 4 and above
-public class HelloCosMessage implements MessageEndEvent
+public class HelloCosMessage implements MessageIntermediateThrowEvent
 {
 	@Override
 	public List<Task.ParameterComponent> getAdditionalInputParameters(ProcessPluginApi api, Variables variables,
