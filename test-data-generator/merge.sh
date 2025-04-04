adadbd5ccb1910e859001f75e545c6b288cebd9b
Jan Böhringer <jan.boehringer@hs-heilbronn.de>
2025-04-04 11:55:48 +0200

    Merge now uses fast-forwarding

--- test-data-generator/merge.sh
+++ test-data-generator/merge.sh
@@ -1,23 +1,23 @@
 git checkout solutions/exercise-1
-git merge main
+git merge --ff main
 
 git checkout solutions/exercise-2
-git merge solutions/exercise-1
+git merge --ff solutions/exercise-1
 
 git checkout solutions/exercise-3
-git merge solutions/exercise-2
+git merge --ff solutions/exercise-2
 
 git checkout solutions/exercise-4
-git merge solutions/exercise-3
+git merge --ff solutions/exercise-3
 
 git checkout solutions/exercise-5
-git merge solutions/exercise-4
+git merge --ff solutions/exercise-4
 
 git checkout solutions/exercise-6
-git merge solutions/exercise-5
+git merge --ff solutions/exercise-5
 
 git checkout solutions/exercise-7
-git merge solutions/exercise-6
+git merge --ff solutions/exercise-6
 
 git checkout skeleton/exercise-7
-git merge solutions/exercise-7
+git merge --ff solutions/exercise-7
