397ddeeea0aaa00e1b35f18ba8d58944ed644f57
Jan Böhringer <jan.boehringer@hs-heilbronn.de>
2025-04-04 10:05:51 +0200

    Fixed wrong use of merge command

--- test-data-generator/merge.sh
+++ test-data-generator/merge.sh
@@ -1,23 +1,23 @@
 git checkout solutions/exercise-1
-git merge main solutions/exercise-1
+git merge main
 
 git checkout solutions/exercise-2
-git merge solutions/exercise-1 solutions/exercise-2
+git merge solutions/exercise-1
 
 git checkout solutions/exercise-3
-git merge solutions/exercise-2 solutions/exercise-3
+git merge solutions/exercise-2
 
 git checkout solutions/exercise-4
-git merge solutions/exercise-3 solutions/exercise-4
+git merge solutions/exercise-3
 
 git checkout solutions/exercise-5
-git merge solutions/exercise-4 solutions/exercise-5
+git merge solutions/exercise-4
 
 git checkout solutions/exercise-6
-git merge solutions/exercise-5 solutions/exercise-6
+git merge solutions/exercise-5
 
 git checkout solutions/exercise-7
-git merge solutions/exercise-6 solutions/exercise-7
+git merge solutions/exercise-6
 
 git checkout skeleton/exercise-7
-git merge solutions/exercise-7 skeleton/exercise-7
+git merge solutions/exercise-7
